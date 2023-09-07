7e7cda2ee047d2abd47678be25a4f0c75c7f9fdd
TeddyGavi <matc.davis@gmail.com>
2023-09-07 15:38:53 -0700

    basic styling

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const http = require('http')
@@ -8,6 +9,8 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+const env = process.env.NODE_ENV
+
 app.use(express.static(path.join(__dirname, "public")))
 
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server listening on port ${PORT} and env is ${env}`))
