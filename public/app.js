7e02358074d5bf252cfe5b4e9d01877a95100111
TeddyGavi <matc.davis@gmail.com>
2023-10-04 16:03:31 -0700

    basic ship grid

--- public/app.js
+++ public/app.js
@@ -1,27 +1,30 @@
 let computerBoard,
   playerBoard = [];
 const gameGrid = document.getElementById("game__grid");
+const shipsContainer = document.getElementById("ship__container");
 const width = 10;
 
-const carrier = {
-  length: 5,
-  orientation: "horizontal",
-};
-const battleship = {
-  length: 4,
-  orientation: "horizontal",
-};
-const crusier = {
-  length: 3,
-  orientation: "horizontal",
-};
-const submarine = {
-  length: 3,
-  orientation: "horizontal",
-};
-const destroyer = {
-  length: 2,
-  orientation: "horizontal",
+const ships = {
+  carrier: {
+    length: 5,
+    orientation: "horizontal",
+  },
+  battleship: {
+    length: 4,
+    orientation: "horizontal",
+  },
+  crusier: {
+    length: 3,
+    orientation: "horizontal",
+  },
+  submarine: {
+    length: 3,
+    orientation: "horizontal",
+  },
+  destroyer: {
+    length: 2,
+    orientation: "horizontal",
+  },
 };
 // from hmtl script
 gameMode === "multiPlayer" ? multiplayerMode() : singleplayerMode();
@@ -31,22 +34,37 @@ function createGrid() {
 }
 
 function fillGridDiv() {
-  console.log(gameGrid);
   if (!gameGrid) return;
   for (let i = 0; i < 100; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid__item");
-    gridItem.textContent = i + 1;
+    gridItem.textContent = i;
     gameGrid.appendChild(gridItem);
+  }
+}
+
+function createShips() {
+  if (!shipsContainer) return;
+
+  for (const oneShip in ships) {
+    const ship = document.createElement("div");
+    let shipSquares;
+    ship.classList.add("ship", oneShip);
+    for (let i = 0; i < ships[oneShip].length; i++) {
+      shipSquares = document.createElement("div");
+      shipSquares.classList.add("ship__square");
+      ship.appendChild(shipSquares);
+    }
+    shipsContainer.appendChild(ship);
   }
 }
 
 function placeShipsRandomly() {}
 function singleplayerMode() {
-  console.log("single player");
   computerBoard = createGrid();
   playerBoard = createGrid();
   fillGridDiv();
+  createShips();
 }
 
 function multiplayerMode() {
